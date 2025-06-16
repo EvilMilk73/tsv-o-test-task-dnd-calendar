1b2cfb757f5f2ca5e245b2729b0c073cc788c7a7
evilmilk <evilmilk00@gmail.com>
2025-06-16 18:51:29 +0300

    fix deploy 9

--- apps/testCalendarFront/vite.config.ts
+++ apps/testCalendarFront/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/", // Change from "./" to "/"
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   server: {
     proxy: {
-      "/api": "http://localhost:3001", // backend API port
+      "/api": "http://localhost:3001",
     },
   },
 });
